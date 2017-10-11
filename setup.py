0768087ddb1909ee7cc7552efe3d44672c87dfae
Audric Perrin <audric.perrin@dune-sa.fr>
2017-10-11 21:08:19 +0200

    Modification nom du .exe

--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@ if sys.platform == "win32":
     base = "Win32GUI"
 
 setup(  name = "DUNE production bobine",
-        version = "4.2",
-        description = "Analyse production de bobines sur la machine MONDON",
+        version = "1.0",
+        description = "Analyse production de bobines sur la machine Mondon",
         options = {"build_exe": build_exe_options},
         executables = [Executable(script="main.py",
                                   base=base,
