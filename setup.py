eb138c8e3f3f52e877f775115f9e446b3f2e28ab
Audric Perrin <audric.perrin@dune-sa.fr>
2017-10-21 12:20:24 +0200

    Update setup.py suite réarchitecture des fichiers

--- setup.py
+++ setup.py
@@ -3,17 +3,17 @@ from cx_Freeze import setup, Executable
 
 build_exe_options = {"packages": ["os"],
                      "excludes": ["tkinter"],
-                     "include_files": ["img", "object", "param", "fonction"]}
+                     "include_files": ["assets", "constants", "lib", "store", "ui"]}
 
 base = None
 if sys.platform == "win32":
     base = "Win32GUI"
 
-setup(  name = "DUNE production bobine",
-        version = "1.0",
-        description = "Analyse production de bobines sur la machine Mondon",
-        options = {"build_exe": build_exe_options},
-        executables = [Executable(script="main.py",
-                                  base=base,
-                                  icon="assets/icons/logo_dune_production.ico",
-                                  targetName="DUNE production bobines.exe")])
+setup(name="DUNE production bobine",
+      version="1.0",
+      description="Analyse production de bobines sur la machine Mondon",
+      options={"build_exe": build_exe_options},
+      executables=[Executable(script="main.py",
+                              base=base,
+                              icon="assets/icons/logo_dune_production.ico",
+                              targetName="DUNE production bobines.exe")])
