d0c49c35af1bb090ece84061344d4c59d5a03b0b
Audric Perrin <audric.perrin@dune-sa.fr>
2017-11-15 17:59:52 +0100

    Ajout nouveau dossier qobject dans setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from cx_Freeze import setup, Executable
 
 build_exe_options = {"packages": ["os"],
                      "excludes": ["tkinter"],
-                     "include_files": ["assets", "constants", "lib", "stores", "ui"]}
+                     "include_files": ["assets", "constants", "lib", "stores", "ui", "qobjects"]}
 
 base = None
 if sys.platform == "win32":
