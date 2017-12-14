b5b3141015b9e4ed2ae9c1194530dd2f19441fc6
Audric Perrin <audric.perrin@dune-sa.fr>
2017-12-14 22:18:02 +0100

    Chart dans rapport

--- ui/widgets/rapport/menu_rapport.py
+++ ui/widgets/rapport/menu_rapport.py
@@ -2,10 +2,42 @@
 # -*- coding: utf-8 -*-
 
 from PyQt5.QtWidgets import QHBoxLayout, QLabel, QVBoxLayout, QScrollArea, QWidget
-from PyQt5.QtGui import QPainter, QFont
+from PyQt5.QtGui import QPainter, QFont, QBrush
 from PyQt5.QtPrintSupport import QPrinter, QPrintDialog
 from PyQt5.QtCore import QSize, Qt, QPoint
+from constants.colors import (
+    color_blanc,
+    color_bleu_gris,
+    color_gris_clair,
+    color_gris_fonce,
+    color_gris_moyen,
+    color_noir,
+    color_rouge,
+    color_vert,
+)
+from constants.dimensions import (
+    chart_margin_bottom,
+    chart_margin_left,
+    chart_margin_right,
+    chart_margin_top,
+    chart_min_hour,
+    chart_max_hour,
+    width_grille,
+)
+from constants.param import VITESSE_LIMITE_ASSIMILATION_ARRET
+from stores.data_store_manager import data_store_manager
+from stores.settings_store import settings_store
+from ui.utils.drawing import draw_rectangle, draw_text
+from ui.utils.timestamp import (
+    hour_in_timestamp,
+    timestamp_at_day_ago,
+    timestamp_at_time,
+    timestamp_au_debut_de_hour,
+)
+from ui.widgets.public.mondon_widget import MondonWidget
 
+from ui.utils.drawing import draw_rectangle, draw_text
+from ui.utils.timestamp import timestamp_at_day_ago
 from ui.widgets.prod.chart.chart import Chart
 from ui.widgets.prod.chart_stat.stat_titre import StatTitre
 from ui.widgets.prod.tableau_arret.tab_arret_menu import TabArretMenu
@@ -18,6 +50,10 @@ from ui.widgets.public.pixmap_button import PixmapButton
 
 class RapportMenu(MondonWidget):
     PIXMAPBUTTON_SIZE = QSize(40, 40)
+    DEC_X = 40
+    DEC_Y = 50
+    CHART_H = 200
+    CHART_W = 480
 
     def __init__(self, parent=None):
         super(RapportMenu, self).__init__(parent=parent)
@@ -52,12 +88,10 @@ class RapportMenu(MondonWidget):
 
     def impression(self):
         printer = QPrinter()
-        # printer.setOutputFileName("print.pdf")
+        printer.setOutputFileName("print.pdf")
         painter = QPainter()
         painter.begin(printer)
-        self.rapport.render(painter, QPoint())
-        # self.rapport.render(painter)
-
+        self.drawing(painter)
         painter.end()
 
     # def impression(self):
@@ -91,10 +125,10 @@ class RapportMenu(MondonWidget):
     def create_rapport(self):
         vbox = QVBoxLayout()
 
-        chart = Chart(parent=self)
-        chart.setFixedHeight(300)
-        chart.setFixedWidth(750)
-        vbox.addWidget(chart)
+        self.chart = Chart(parent=self)
+        self.chart.setFixedHeight(300)
+        self.chart.setFixedWidth(750)
+        vbox.addWidget(self.chart)
 
         stat_menu = StatTitre(parent=self, note=False)
         vbox.addWidget(stat_menu)
@@ -107,3 +141,89 @@ class RapportMenu(MondonWidget):
 
         return rapport
 
+    def draw_background(self, p):
+        draw_rectangle(p, self.DEC_X, self.DEC_Y, self.CHART_W, self.CHART_H, color_gris_clair)
+
+    def draw_speed(self, p):
+
+        def get_speed():
+            speeds = data_store_manager.get_current_store().data
+            i = 0
+            current_sum = 0
+            data_120 = []
+            for speed in speeds:
+                if i < 120:
+                    value = speed[1]
+                    current_sum += value
+                else:
+                    i = 0
+                    data_120.append(current_sum / 120)
+                    current_sum = 0
+                i += 1
+            data_120.append(round(current_sum / 120))
+            return data_120
+
+        speeds = get_speed()
+        i = 0
+        for speed in speeds:
+            color = color_vert if speed > VITESSE_LIMITE_ASSIMILATION_ARRET else color_rouge
+            draw_rectangle(p, self.DEC_X + i, self.CHART_H - speed + self.DEC_Y, 1, speed + 1, color)
+            i += 1
+
+    def draw_border(self, p):
+        draw_rectangle(p, self.DEC_X, self.DEC_Y, 1, self.CHART_H, color_bleu_gris)
+        draw_rectangle(p, self.DEC_X, self.DEC_Y, self.CHART_W, 1, color_bleu_gris)
+        draw_rectangle(p, self.DEC_X + self.CHART_W, self.DEC_Y, 1, self.CHART_H + 1, color_bleu_gris)
+        draw_rectangle(p, self.DEC_X, self.DEC_Y + self.CHART_H, self.CHART_W, 1, color_bleu_gris)
+
+    def draw_v_grid(self, p):
+        i = 0
+        hour = 6
+        while i <= 32:
+            dec_hour = 3 if i % 2 == 0 else 0
+            color = color_gris_moyen if i % 2 == 0 else color_gris_clair
+            draw_rectangle(p, self.DEC_X + (15 * i), self.DEC_Y, 1, self.CHART_H + 5 + dec_hour, color)
+            if i % 2 == 0:
+                draw_text(p,
+                          x=self.DEC_X + (15 * i) - 25,
+                          y=self.DEC_Y + self.CHART_H + 5,
+                          width=50,
+                          height=20,
+                          color=color_noir,
+                          align="C",
+                          font_size=8,
+                          text="{}:00".format(hour))
+                hour += 1
+            i += 1
+
+    def draw_h_grid(self, p):
+        i = 0
+        speed = 0
+        while i <= 4:
+            speed = 180 if i == 4 else speed
+            color = color_gris_clair if i < 4 else color_gris_moyen
+            draw_rectangle(p,
+                           self.DEC_X - 3,
+                           self.CHART_H - speed + self.DEC_Y,
+                           self.CHART_W + 3,
+                           1,
+                           color)
+            draw_text(p,
+                      x=self.DEC_X - 35,
+                      y=self.CHART_H - speed + self.DEC_Y - 10,
+                      width=30,
+                      height=20,
+                      color=color_noir,
+                      align="D",
+                      font_size=8,
+                      text=str(speed))
+            speed += 50
+            i += 1
+
+    def drawing(self, p):
+        # self.draw_global_border(p)
+        # ____DRAW_CHART___
+        self.draw_v_grid(p)  # Grille verticale & légende
+        self.draw_h_grid(p)  # Grille horizontale & légende
+        self.draw_speed(p)  # Les vitesses
+        self.draw_border(p)  # La bordure
