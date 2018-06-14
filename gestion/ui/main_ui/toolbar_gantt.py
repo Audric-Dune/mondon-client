1029914cb6334f39cf592e4f77def94918e5b635
audric-perrin <audric.perrin@gmail.com>
2018-06-14 21:54:48 +0200

    toolbar fonction add OK

--- gestion/ui/main_ui/toolbar_gantt.py
+++ gestion/ui/main_ui/toolbar_gantt.py
@@ -3,12 +3,14 @@
 
 from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel, QHBoxLayout
 from PyQt5.QtGui import QPixmap, QPen, QPainter, QColor, QBrush
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QPoint, pyqtSignal
 
-from commun.constants.colors import color_noir, color_blanc, color_vert_fonce, color_vert_moyen
+from commun.constants.colors import color_noir, color_blanc, color_vert_fonce, color_vert_moyen, color_gris_moyen
 from commun.constants.stylesheets import black_12_label_stylesheet,\
     white_12_no_bg_label_stylesheet,\
     gray_clair_12_no_bg_label_stylesheet
+
+from gestion.stores.settings_store import settings_store_gestion
 
 
 class ToolbarGantt(QWidget):
@@ -47,14 +49,11 @@ class ToolbarGantt(QWidget):
 
     def init_bt(self):
         self.bt_edit.set_disabled(True)
-        # BOUTON AJOUTER
-        # self.bt_add.set_drop_down()
-        # self.bt_add.add_item_drop_down(name="plan_prod", literral_name="Plan de production")
-        # self.bt_add.add_item_drop_down(name="clean", literral_name="Nettoyage machine")
-        # self.bt_add.add_item_drop_down(name="tool", literral_name="Action de maintenance")
-        # self.bt_add.add_item_drop_down(name="stop", literral_name="Arrêt production")
-        # self.bt_add.SELECT_ITEM.connect(self._handle_select_item_from_add)
-        pass
+        self.bt_add.set_dropdown(callback=self._handle_select_item_from_add)
+        self.bt_add.add_item_drop_down(name="plan_prod", literral_name="Plan de production")
+        self.bt_add.add_item_drop_down(name="clean", literral_name="Nettoyage machine")
+        self.bt_add.add_item_drop_down(name="tool", literral_name="Action de maintenance")
+        self.bt_add.add_item_drop_down(name="stop", literral_name="Arrêt production")
 
     def paintEvent(self, e):
         p = QPainter(self)
@@ -73,29 +72,33 @@ class ToolbarGantt(QWidget):
         p.drawRect(0, 0, self.width()-1, self.height()-1)
 
     def _handle_select_item_from_add(self, item_selected_name):
-        print("_handle_select_item_from_add")
-        pass
-
+        if item_selected_name == "plan_prod":
+            settings_store_gestion.create_new_plan()
+        else:
+            settings_store_gestion.create_new_event(item_selected_name)
 
 class ButtonMenu(QWidget):
 
-    def __init__(self, text, icon, icon_disabled, icon_hover, parent=None):
+    def __init__(self, text, icon, icon_disabled, icon_hover, parent=None, callback=None):
         super(ButtonMenu, self).__init__(parent=parent)
         self.setFixedWidth(70)
+        self.vbox = QVBoxLayout()
         self.label = QLabel(text)
         self.pixmap_icon = QLabel()
         self.pixmap_icon.setFixedSize(30, 30)
         self.icon = QPixmap(icon)
         self.icon_disabled = QPixmap(icon_disabled)
         self.icon_hover = QPixmap(icon_hover)
+        self.dropdown = None
         self.background_color = color_blanc
         self.hover = False
         self.button_press = False
         self.disabled = False
-        self.init_ui()
+        self.popup_show = False
+        self.callback = callback
+        self.init_ui(self.vbox)
 
-    def init_ui(self):
-        vbox = QVBoxLayout()
+    def init_ui(self, vbox):
         vbox.setContentsMargins(0, 5, 0, 5)
         vbox.setSpacing(0)
         self.setLayout(vbox)
@@ -121,7 +124,13 @@ class ButtonMenu(QWidget):
         p.fillRect(1, 1, self.width()-2, self.height()-2, qcolor)
 
     def update_ui(self):
-        if self.disabled:
+        if self.dropdown is not None:
+            self.dropdown.update_ui()
+        if self.popup_show:
+            self.background_color = color_vert_moyen
+            self.pixmap_icon.setPixmap(self.icon_hover)
+            self.label.setStyleSheet(white_12_no_bg_label_stylesheet)
+        elif self.disabled:
             self.background_color = color_blanc
             self.pixmap_icon.setPixmap(self.icon_disabled)
             self.label.setStyleSheet(gray_clair_12_no_bg_label_stylesheet)
@@ -139,9 +148,25 @@ class ButtonMenu(QWidget):
             self.label.setStyleSheet(black_12_label_stylesheet)
         self.update()
 
+    def set_dropdown(self, callback):
+        self.dropdown=DropDown(parent=self)
+        self.dropdown.SELECT_ITEM_SIGNAL.connect(callback)
+        self.vbox.addWidget(self.dropdown)
+
+    def add_item_drop_down(self, name, literral_name):
+        if self.dropdown is None:
+            return
+        self.dropdown.popup.add_item(name=name, literral_name=literral_name)
+
     def set_disabled(self, p_bool):
         self.disabled = p_bool
         self.update_ui()
+
+    def _handle_button_press(self):
+        if self.dropdown:
+            self.dropdown.show_popup()
+        elif self.callback is not None:
+            self.callback()
 
     def enterEvent(self, e):
         self.hover = True
@@ -154,11 +179,163 @@ class ButtonMenu(QWidget):
         super(ButtonMenu, self).enterEvent(e)
 
     def mousePressEvent(self, e):
-        self.button_press = True
-        self.update_ui()
+        if e.button() == Qt.LeftButton:
+            self.button_press = True
+            self.update_ui()
         super(ButtonMenu, self).enterEvent(e)
 
     def mouseReleaseEvent(self, e):
-        self.button_press = False
-        self.update_ui()
+        if e.button() == Qt.LeftButton:
+            self.button_press = False
+            self._handle_button_press()
+            self.update_ui()
         super(ButtonMenu, self).enterEvent(e)
+
+
+class DropDown(QWidget):
+    SELECT_ITEM_SIGNAL = pyqtSignal(str)
+
+    def __init__(self, parent):
+        super(DropDown, self).__init__(parent=parent)
+        self.parent = parent
+        self.hover = False
+        self.button_press = False
+        self.disabled = False
+        self.popup_show = False
+        self.background_color = color_blanc
+        self.pixmap_icon = QLabel()
+        self.pixmap_icon.setFixedSize(15, 15)
+        self.icon = QPixmap("commun/assets/images/arrow.png")
+        self.icon_disabled = QPixmap("commun/assets/images/arrow_disabled.png")
+        self.icon_hover = QPixmap("commun/assets/images/arrow_hover.png")
+        self.popup = Popup()
+        self.popup.HIDE_SIGNAL.connect(self._handle_hide_popup)
+        self.popup.SELECT_ITEM_SIGNAL.connect(self._handle_select_item)
+        self.init_ui()
+        self.update_ui()
+
+    def init_ui(self):
+        hbox = QHBoxLayout()
+        hbox.setContentsMargins(0, 0, 0, 0)
+        self.pixmap_icon.setAlignment(Qt.AlignCenter)
+        self.pixmap_icon.setScaledContents(True)
+        hbox.addWidget(self.pixmap_icon)
+        self.setLayout(hbox)
+
+    def update_ui(self):
+        self.popup_show = self.parent.popup_show
+        self.hover = self.parent.hover
+        self.button_press = self.parent.button_press
+        self.disabled = self.parent.disabled
+        if self.popup_show:
+            self.background_color = color_vert_moyen
+            self.pixmap_icon.setPixmap(self.icon_hover)
+        elif self.disabled:
+            self.background_color = color_blanc
+            self.pixmap_icon.setPixmap(self.icon_disabled)
+        elif self.button_press:
+            self.background_color = color_vert_moyen
+            self.pixmap_icon.setPixmap(self.icon_hover)
+        elif self.hover:
+            self.background_color = color_vert_fonce
+            self.pixmap_icon.setPixmap(self.icon_hover)
+        else:
+            self.background_color = color_blanc
+            self.pixmap_icon.setPixmap(self.icon)
+        self.update()
+
+    def _handle_hide_popup(self):
+        self.parent.popup_show = False
+        self.parent.update_ui()
+
+    def _handle_select_item(self, name):
+        self.SELECT_ITEM_SIGNAL.emit(name)
+
+    def show_popup(self):
+        pos_popup = self.mapToGlobal(QPoint(0, self.height()))
+        self.popup.move(pos_popup)
+        self.parent.popup_show = True
+        self.parent.update_ui()
+        self.popup.show()
+
+
+class Popup(QWidget):
+    SELECT_ITEM_SIGNAL = pyqtSignal(str)
+    HIDE_SIGNAL = pyqtSignal()
+
+    def __init__(self):
+        super(Popup, self).__init__()
+        self.setWindowFlag(Qt.Popup)
+        self.vbox = QVBoxLayout()
+        self.init_ui()
+
+    def init_ui(self):
+        self.vbox.setContentsMargins(1, 1, 1, 1)
+        self.vbox.setSpacing(0)
+        self.setLayout(self.vbox)
+
+    def paintEvent(self, e):
+        p = QPainter(self)
+        color = color_gris_moyen.rgb_components
+        qcolor = QColor(color[0], color[1], color[2])
+        pen = QPen()
+        pen.setColor(qcolor)
+        p.setPen(pen)
+        p.drawRect(0, 0, self.width()-1, self.height()-1)
+
+    def add_item(self, name, literral_name):
+        new_line = LineItem(name=name)
+        new_line.CLICKED_SIGNAL.connect(self._handle_select_item)
+        new_line.setText(literral_name)
+        self.vbox.addWidget(new_line)
+        self.update()
+
+    def _handle_select_item(self, name):
+        self.SELECT_ITEM_SIGNAL.emit(name)
+        self.hide()
+
+    def hideEvent(self, e):
+        self.HIDE_SIGNAL.emit()
+        super(Popup, self).hideEvent(e)
+
+
+class LineItem(QLabel):
+    CLICKED_SIGNAL = pyqtSignal(str)
+
+    def __init__(self, name):
+        super(LineItem, self).__init__()
+        self.name = name
+        self.background_color = color_blanc
+        self.setStyleSheet(black_12_label_stylesheet)
+        self.setFixedHeight(20)
+
+    def paintEvent(self, e):
+        p = QPainter(self)
+        color = self.background_color.rgb_components
+        qcolor = QColor(color[0], color[1], color[2])
+        pen = QPen()
+        pen.setColor(qcolor)
+        p.setPen(pen)
+        p.fillRect(0, 0, self.width(), self.height(), qcolor)
+        super(LineItem, self).paintEvent(e)
+
+    def mousePressEvent(self, e):
+        self.background_color = color_vert_moyen
+        self.update()
+        super(LineItem, self).mousePressEvent(e)
+
+    def mouseReleaseEvent(self, e):
+        self.CLICKED_SIGNAL.emit(self.name)
+        super(LineItem, self).mouseReleaseEvent(e)
+
+    def enterEvent(self, e):
+        self.setStyleSheet(white_12_no_bg_label_stylesheet)
+        self.background_color = color_vert_fonce
+        self.update()
+        super(LineItem, self).enterEvent(e)
+
+    def leaveEvent(self, e):
+        self.setStyleSheet(black_12_label_stylesheet)
+        self.background_color = color_blanc
+        self.update()
+        super(LineItem, self).enterEvent(e)
