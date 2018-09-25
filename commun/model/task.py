8861086a877a0dd57b52bc03e01b9d2d6b104a45
Audric Perrin <audric.perrin@dune-sa.fr>
2018-09-25 16:53:37 +0200

    gestion insertion multiday v2

--- commun/model/task.py
+++ commun/model/task.py
@@ -4,21 +4,22 @@
 
 class Task:
 
-    def __init__(self, start, end, plan_prod=None, event=None):
+    def __init__(self, start, end, plan_prod=None, event=None, day_ago=None):
         self.start = start
         self.end = end
         self.plan_prod = plan_prod
         self.event = event
         self.index = None
+        self.day_ago = day_ago
 
     def get_start(self):
         return self.start
 
     def get_index(self):
-        return self.index
+        return self.index - (self.day_ago*100)
 
     def __repr__(self):
         if self.plan_prod:
-            return "Index: {} Plan production: {}".format(self.index, self.plan_prod)
+            return "Index: {}, Day_ago: {}, Plan production: {}".format(self.index, self.day_ago, self.plan_prod)
         else:
             return str(self.event)
--- gestion/stores/settings_store.py
+++ gestion/stores/settings_store.py
@@ -86,7 +86,6 @@ class SettingsStore(QObject):
     def set_item_focus(self, item):
         from commun.model.plan_prod import PlanProd
         if self.standing_insert and isinstance(item, PlanProd):
-            print("INSERTION : ", self.standing_insert, "___ AVANT : ", item)
             self.insert_plan_prod_1_before_plan_prod_2(plan_1=self.standing_insert, plan_2=item)
             self.standing_insert = None
             self.focus = None
@@ -96,21 +95,27 @@ class SettingsStore(QObject):
             self.FOCUS_CHANGED_SIGNAL.emit()
 
     def insert_plan_prod_1_before_plan_prod_2(self, plan_1, plan_2):
-        def set_index_to_plan_prod(p_index, plan_prod):
+        def set_index_to_plan_prod(p_index, plan_prod, day_ago=None):
             for p_task in tasks:
                 if p_task.plan_prod == plan_prod:
                     p_task.index = p_index
+                    if day_ago is not None:
+                        p_task.day_ago = day_ago
         tasks = self.get_tasks_at_day_ago(day_ago=self.day_ago)
         plan_1_in_tasks = False
         for task in tasks:
             if task.plan_prod == plan_1:
                 plan_1_in_tasks = True
         if not plan_1_in_tasks:
-            tasks.append(Task(plan_prod=plan_1, start=plan_1.start, end=plan_1.end))
+            tasks.append(Task(plan_prod=plan_1, start=plan_1.start, end=plan_1.end, day_ago=self.day_ago))
         index = 0
+        current_day_ago = self.day_ago
         for task in tasks:
+            if current_day_ago != task.day_ago:
+                current_day_ago = task.day_ago
+                index = 0
             if task.plan_prod == plan_2:
-                set_index_to_plan_prod(plan_prod=plan_1, p_index=index)
+                set_index_to_plan_prod(plan_prod=plan_1, p_index=index, day_ago=current_day_ago)
                 index += 1
                 set_index_to_plan_prod(plan_prod=plan_2, p_index=index)
                 index += 1
@@ -118,7 +123,6 @@ class SettingsStore(QObject):
                 task.index = index
                 index += 1
         tasks = sorted(tasks, key=lambda t: t.get_index())
-        print(tasks)
         self.update_plans_prods(tasks=tasks)
 
     def set(self, day_ago=None, plan_prod=None):
@@ -152,222 +156,58 @@ class SettingsStore(QObject):
         for bobine_poly in bobine_poly_store.bobines:
             bobine_poly.get_stock_at_time(time=self.plan_prod.start)
 
-    # def get_new_start(self, start, plans_prods):
-    #     if plans_prods is None:
-    #         from gestion.stores.plan_prod_store import plan_prod_store
-    #         plans_prods = plan_prod_store.plans_prods
-    #     for prod in plans_prods:
-    #         if prod.start == start:
-    #             return prod.end
-    #     for event in event_store.events:
-    #         if event.start == start:
-    #             return self.get_end_at_start(start, end=event.end)
-    #     return False
-
     @staticmethod
     def get_tasks_at_day_ago(day_ago):
         tasks = []
         from gestion.stores.plan_prod_store import plan_prod_store
         for plan_prod in plan_prod_store.plans_prods:
             if plan_prod.start > timestamp_at_day_ago(day_ago):
-                tasks.append(Task(start=plan_prod.start, plan_prod=plan_prod, end=plan_prod.end))
-        from gestion.stores.event_store import event_store
-        for event in event_store.events:
-            if event.start > timestamp_at_day_ago(day_ago):
-                tasks.append(Task(start=event.start, event=event, end=event.end))
+                if plan_prod.start > timestamp_at_day_ago(day_ago-1):
+                    day_ago -= 1
+                tasks.append(Task(start=plan_prod.start, plan_prod=plan_prod, end=plan_prod.end, day_ago=day_ago))
         tasks = sorted(tasks, key=lambda t: t.get_start())
         index = 0
+        day_ago = None
         for task in tasks:
+            if day_ago is None:
+                day_ago = task.day_ago
+            if task.day_ago < day_ago:
+                day_ago = task.day_ago
+                index = 0
             task.index = index
             index += 1
         return tasks
 
     def update_plans_prods(self, tasks=None):
-        def get_end_from_index_task(index):
+        def get_end_from_index_task(index, p_day_ago):
             for p_task in tasks:
-                if p_task.index == index:
+                if p_task.index == index and p_task.day_ago == p_day_ago:
                     return p_task.plan_prod.end
 
-        def get_task_from_index(index):
+        def get_task_from_index(index, p_day_ago):
             for p_task in tasks:
-                if p_task.index == index:
+                if p_task.index == index and p_task.day_ago == p_day_ago:
                     return p_task
+        current_day_ago = self.day_ago
         if tasks is None:
             tasks = self.get_tasks_at_day_ago(day_ago=self.day_ago)
         for task in tasks:
-            start_day = timestamp_at_time(timestamp_at_day_ago(day_ago=self.day_ago), hours=DEBUT_PROD_MATIN)
             if task.index == 0:
+                if current_day_ago != task.day_ago:
+                    current_day_ago = task.day_ago
                 from gestion.stores.plan_prod_store import plan_prod_store
+                start_day = timestamp_at_time(timestamp_at_day_ago(day_ago=current_day_ago), hours=DEBUT_PROD_MATIN)
                 last_plan_prod = plan_prod_store.get_last_plan_prod(start_plan_prod=start_day)
                 task.plan_prod.update_from_start(start=start_day, last_plan_prod=last_plan_prod)
             else:
-                last_plan_prod = get_task_from_index(task.index-1).plan_prod
-                start_task = task.plan_prod.start
-                if get_min_in_timestamp(start_task) == 0 and get_hour_in_timestamp(start_task) == DEBUT_PROD_MATIN and start_task != start_day:
-                    start = start_task
-                else:
-                    start = get_end_from_index_task(index=task.index-1)
+                last_plan_prod = get_task_from_index(task.index-1, p_day_ago=current_day_ago).plan_prod
+                start = get_end_from_index_task(index=task.index-1, p_day_ago=current_day_ago)
                 task.plan_prod.update_from_start(start=start, last_plan_prod=last_plan_prod)
             self.update_plan_prod_on_database(plan_prod=task.plan_prod)
         self.SETTINGS_CHANGED_SIGNAL.emit()
-        # self.cursor = None
-        # plans_prods = plan_prod_store.plans_prods
-        # plans_prods_update = []
-        # last_plan_prod = None
-        # for plan_prod in plans_prods:
-        #     self.set_plan_prod(plan_prod, plans_prods_update)
-        #     if last_plan_prod is None:
-        #         continue
-        #     else:
-        #         if self.is_meltable_plans_prods(plan_prod_1=last_plan_prod, plan_prod_2=plan_prod):
-        #             self.merge_plan_prod(plan_prod_1=last_plan_prod, plan_prod_2=plan_prod)
-        #             del plans_prods_update[-2:-1]
-        #             plans_prods_update.append(last_plan_prod)
-        #     last_plan_prod = plan_prod
-        # self.merge_plans_prods(plans_prods_update)
-        # self.SETTINGS_CHANGED_SIGNAL.emit()
 
-    # def merge_plans_prods(self, plans_prods_update):
-    #     last_plan_prod = None
-    #     for plan_prod in plans_prods_update:
-    #         if last_plan_prod is None:
-    #             last_plan_prod = plan_prod
-    #         else:
-    #             if self.is_meltable_plans_prods(plan_prod_1=last_plan_prod, plan_prod_2=plan_prod):
-    #                 self.merge_plan_prod(plan_prod_1=last_plan_prod, plan_prod_2=plan_prod)
-    #             else:
-    #                 last_plan_prod = plan_prod
-    #
-    # def merge_plan_prod(self, plan_prod_1, plan_prod_2):
-    #     plan_prod_1.end = plan_prod_2.end
-    #     plan_prod_1.tours += plan_prod_2.tours
-    #     self.update_plan_prod_on_database(plan_prod_1)
-    #     self.delete_plan_prod(plan_prod_2, update=False)
-    #
-    # def is_meltable_plans_prods(self, plan_prod_1, plan_prod_2):
-    #     code_bob_1 = self.get_code_bobine_selected(plan_prod_1.bobines_filles_selected)
-    #     code_bob_2 = self.get_code_bobine_selected(plan_prod_2.bobines_filles_selected)
-    #     if code_bob_1 == code_bob_2 and plan_prod_1.end == plan_prod_2.start:
-    #         return True
-    #     return False
-    #
-    # def set_plan_prod(self, plan_prod, plans_prods_update):
-    #     plan_prod.start = self.get_start(plans_prods=plans_prods_update)
-    #     plan_prod.get_end()
-    #     start_split = self.is_there_an_event_or_end_day_in_plan_prod(plan_prod)
-    #     if start_split:
-    #         self.split_plan_prod(plan_prod, start_split=start_split, plans_prods_update=plans_prods_update)
-    #     else:
-    #         self.cursor = plan_prod.end
-    #         self.update_plan_prod_on_database(plan_prod)
-    #         plans_prods_update.append(plan_prod)
-    #
-    # def split_plan_prod(self, plan_prod, start_split, plans_prods_update):
-    #     total_tours = plan_prod.tours
-    #     plan_prod.tours = plan_prod.get_max_tour(end=start_split)
-    #     self.set_plan_prod(plan_prod, plans_prods_update)
-    #     from commun.model.plan_prod import PlanProd
-    #     new_plan_prod = PlanProd(start=None)
-    #     new_plan_prod.get_plan_prod_param(plan_prod)
-    #     new_plan_prod.tours = total_tours-plan_prod.tours
-    #     self.create_new_plan_prod(new_plan_prod)
-    #     p_id = Database.get_last_id_plan_prod()
-    #     new_plan_prod.p_id = p_id
-    #     self.set_plan_prod(new_plan_prod, plans_prods_update)
-    #
-    # @staticmethod
-    # def is_there_an_event_or_end_day_in_plan_prod(plan_prod):
-    #     from gestion.stores.event_store import event_store
-    #     for event in event_store.events:
-    #         if plan_prod.end > event.start > plan_prod.start:
-    #             return event.start
-    #     if plan_prod.end > timestamp_at_time(plan_prod.start, hours=FIN_PROD_SOIR):
-    #         return timestamp_at_time(plan_prod.start, hours=FIN_PROD_SOIR)
-    #     return False
-    #
-    # def create_new_plan(self):
-    #     print("create_new_plan")
-    #     from commun.model.plan_prod import PlanProd
-    #     start_prod = self.get_start()
-    #     if start_prod:
-    #         plan_prod = PlanProd(start=start_prod)
-    #         self.set(plan_prod=plan_prod)
-    #         plan_prod.init_new_plan()
-    #         self.update_stock_bobines_at_time()
-    #         self.CREATE_PLAN_PROD_WINDOW.emit()
-    #
-    # def edit_plan_prod(self, plan_prod):
-    #     self.plan_prod = plan_prod
-    #     self.update_stock_bobines_at_time()
-    #     self.CREATE_PLAN_PROD_WINDOW.emit()
-    #
-    # def get_start(self, start=None, plans_prods=None):
-    #     if start is None:
-    #         today = timestamp_at_day_ago(0)
-    #         start = timestamp_at_time(ts=today, hours=DEBUT_PROD_MATIN) if self.cursor is None else self.cursor
-    #     new_start = self.get_new_start(start=start, plans_prods=plans_prods)
-    #     if new_start:
-    #         return self.get_start(start=new_start, plans_prods=plans_prods)
-    #     else:
-    #         max_end = self.get_max_end(start)
-    #         if max_end - start < 900:
-    #             if max_end == timestamp_at_time(ts=max_end, hours=FIN_PROD_SOIR):
-    #                 day_ago = 3 if is_vendredi(start) else 1
-    #                 new_start = timestamp_after_day_ago(start=start, day_ago=day_ago, hour=DEBUT_PROD_MATIN)
-    #                 return self.get_start(start=new_start, plans_prods=plans_prods)
-    #             return self.get_start(start=max_end, plans_prods=plans_prods)
-    #
-    # def set_day_ago(self, day_ago):
-    #     # Test si nouveau jour est un samedi ou dimanche
-    #     new_day = timestamp_at_day_ago(day_ago)
-    #     week_day = datetime.fromtimestamp(new_day).weekday()
-    #     if 5 <= week_day <= 6:
-    #         if self.day_ago < day_ago:
-    #             self.set_day_ago(day_ago+1)
-    #         else:
-    #             self.set_day_ago(day_ago-1)
-    #     else:
-    #         self.set(day_ago=day_ago)
-    #     self.ON_DAY_CHANGED.emit()
-    #
-    # def cancel_plan_prod(self):
-    #     self.plan_prod = None
-    #     self.SETTINGS_CHANGED_SIGNAL.emit()
-    #
     def on_data_reglage_changed(self):
         self.plan_prod.ON_CHANGED_SIGNAL.emit()
-    #
-    # @staticmethod
-    # def get_end_at_start(start, end):
-    #     from gestion.stores.event_store import event_store
-    #     end = end
-    #     for event in event_store.events:
-    #         if end >= event.start >= start:
-    #             if event.end > end:
-    #                 end = event.end
-    #     return end
-    #
-    # @staticmethod
-    # def get_max_end(start):
-    #     from gestion.stores.event_store import event_store
-    #     next_start_event = timestamp_at_time(ts=start, hours=FIN_PROD_SOIR)
-    #     end_day = timestamp_at_time(ts=start, hours=FIN_PROD_SOIR)
-    #     for event in event_store.events:
-    #         if event.start > end_day or event.start < start:
-    #             continue
-    #         elif next_start_event is None:
-    #             next_start_event = event.start
-    #         elif next_start_event > event.start:
-    #             next_start_event = event.start
-    #     return next_start_event
-    #
-    # def create_new_event(self, type_event):
-    #     self.CREATE_EVENT_CONFIG_WINDOW.emit(type_event)
-    #
-    # def edit_event(self, event):
-    #     self.CREATE_EVENT_CONFIG_EDIT_WINDOW.emit(event)
-    #
-    # @staticmethod
 
     def save_plan_prod(self):
         if not self.plan_prod.new_plan:
